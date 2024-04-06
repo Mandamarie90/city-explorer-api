aa918437174f31c6d2a4dcb54540762b819fb818
Amanda <amandamarquez27@icloud.com>
2024-04-05 21:07:15 -0700

    Adding Movie API and caching

--- modules/movies.js
+++ modules/movies.js
@@ -1,26 +1,38 @@
-'use strict';
+const axios = require('axios');
 
-async function getMovies( req,res ) {
+async function getMovies(req, res) {
+  const url = 'https://api.themoviedb.org/3/discover/movie';
+  const apiKey = '194690844b4365335d34b2abf0f2929b'; // Replace with your MovieDB API key
+  const options = {
+    headers: {
+      accept: 'application/json',
+    }
+  };
 
-  let fakeMovies = [
-    {title:'We are all mad here'},
-    {title:'You are all going to die down here'},
-    {title:'What is this? Not a house!'}
-  ];
+  try {
+    const response = await axios.get(url, {
+      ...options,
+      params: {
+        api_key: apiKey
+      }
+    });
+    const data = response.data;
+  
+    const movies = data.results.map(movie => new Movie(movie));
 
-  let movies = fakeMovies.map( movie => {
-    return new Movie(movie);
-
-  });
-
-  res.json(movies);
+    res.json(movies);
+  } catch (error) {
+    console.error('error:', error);
+    res.status(500).json({ error: 'Error fetching movies' });
+  }
 }
 
 class Movie {
   constructor(movieData) {
     this.title = movieData.title;
-    this.year = movieData.year;
-    this.posterImage = movieData.posterImage;
+    this.year = movieData.release_date ? new Date(movieData.release_date).getFullYear() : null;
+    this.posterImage = movieData.poster_path ? `https://image.tmdb.org/t/p/w500${movieData.poster_path}` : null;
   }
 }
+
 module.exports = getMovies;
--- modules/weather.js
+++ modules/weather.js
@@ -3,10 +3,9 @@
 const axios = require('axios');
 
 async function getWeather(req, res) {
-  let lat =req.query.latitude;
+  let lat = req.query.latitude;
   let lon = req.query.longitude;
   let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
-
   let axiosResponse = await axios.get(url);
   let weatherData = axiosResponse.data;
   let weather = weatherData.data.map( day => {
